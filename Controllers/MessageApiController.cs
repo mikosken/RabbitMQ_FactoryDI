e9eab23f70baa3036cc0854587963bd3c23ab74c
mikosken <89968969+mikosken@users.noreply.github.com>
2022-10-04 16:33:01 +0200

    feat: Added Swagger xml documentation. feat: Created controller actions for sending and receiving messages using RabbitMQ.

--- Controllers/MessageApiController.cs
+++ Controllers/MessageApiController.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ_FactoryDI.MQFactory;
 
 namespace RabbitMQ_FactoryDI.Controllers
 {
@@ -11,24 +8,44 @@ namespace RabbitMQ_FactoryDI.Controllers
     public class MessageApiController : ControllerBase
     {
         private readonly ILogger<MessageApiController> _logger;
+        private readonly IMessageQueueFactory _messageQueues;
 
-        public MessageApiController(ILogger<MessageApiController> logger)
+        public MessageApiController(ILogger<MessageApiController> logger, IMessageQueueFactory messageQueues)
         {
             _logger = logger;
+            _messageQueues = messageQueues;
         }
 
+        /// <summary>
+        /// Attempts to fetch a single message from RabbitMQ.
+        /// </summary>
+        /// <returns>A message string if there was one available, otherwise null.</returns>
+        /// <response code="200">Returns a message.</response>
+        /// <response code="204">A message was not available.</response>
         [HttpGet(Name = "GetMessage")]
         public IActionResult Get()
         {
             // Retrieve message from RabbitMQ.
+            var q = _messageQueues.GetQueue("MyReceiveQueue");
+            var message = q.Get();
 
-            return StatusCode(StatusCodes.Status200OK, "Message");
+            if (message != null)
+                return StatusCode(StatusCodes.Status200OK, message);
+
+            return StatusCode(StatusCodes.Status204NoContent);
         }
 
+        /// <summary>
+        /// Publishes a string message to RabbitMQ.
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="202">The message was accepted.</response>
         [HttpPost(Name = "PostMessage")]
         public IActionResult Post(string message)
         {
             // Send message to RabbitMQ.
+            var q = _messageQueues.GetQueue("MySendQueue");
+            q.Publish(message);
 
             return StatusCode(StatusCodes.Status202Accepted);
         }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using RabbitMQ_FactoryDI.MQFactory;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,12 @@ builder.Services.AddMessageQueues();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {   
+        // Set the comments path for the Swagger JSON and UI.    
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";  
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);  
+        c.IncludeXmlComments(xmlPath);  
+    });
 
 var app = builder.Build();
 
